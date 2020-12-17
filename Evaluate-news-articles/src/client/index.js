069ec538a038bef6c3abbde7b99a45c62638f726
Shrouk Saadon <69605056+Shroouk@users.noreply.github.com>
2020-12-17 22:35:37 +0200

    Add files via upload

--- Evaluate-news-articles/src/client/index.js
+++ Evaluate-news-articles/src/client/index.js
@@ -1,1 +1,10 @@
+import { handleSubmit } from './js/formHandler'
 
+import './styles/resets.scss';
+import './styles/base.scss';
+import './styles/footer.scss';
+import './styles/form.scss';
+import './styles/header.scss';
+
+
+export { handleSubmit };
